865eb1744471fb5fccc9e7699c0bd1fd6d20705c
bogdan-basuc <b.bogdan913@gmail.com>
2016-11-14 18:08:53 +0200

    Multiplayer update
    implemented mr hide and dr jekyl collisions, score for every player and the random chooser for bad character.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,49 +3,139 @@ using System.Collections;
 using System;
 using Random = UnityEngine.Random;
 using UnityEngine.Networking;
+using UnityEngine.Networking.NetworkSystem;
 
 public class Player : NetworkBehaviour
 {
-    public float moveTime = 0.1f;
     public LayerMask blockingLayer;
     public Sprite[] playerSpriter;
+    public Sprite[] enemySpriter;
     private BoxCollider2D boxCollider;
     private Rigidbody2D rb2D;
-    private float inverseMoveTime;
+    [SyncVar]
+    public float inverseMoveTime;
     private bool moving = false;
 
+    public int firstMessage = -1;
+
+    [SyncVar]
     public int timer = 0;
-    private SpriteManager sm;
+
+    [SyncVar]
+    public int playerLocalSprite = 0;
+
+    [SyncVar]
+    public int mrHide = 0;
+
+    [SyncVar]
+    public int score = 0;
+    private const short chatMessage = 131;
+
+    [SyncVar]
+    public int currentId = 0;
+    [SyncVar]
+    public int totalIds = 1;
+
+    [Command]
+    public void CmdSetCurrentId(int id)
+    {
+        currentId = id;
+    }
+
+    [Command]
+    public void CmdSetTotalId(int total)
+    {
+        totalIds = total;
+    }
+    [Command]
+    public void CmdInitScore()
+    {
+        score = 0;
+    }
+    [Command]
+    public void CmdIncScore()
+    {
+        score++;
+    }
+
+    public int currentHide = 0;
+    public int aaaa = -1;
 
 
+    private void ReceiveMessage(NetworkMessage message)
+    {
+        //reading message
+        aaaa = message.ReadMessage<IntegerMessage>().value;
+
+        if (aaaa == currentId)
+        {
+            CmdChangeSpeed(20.0f);
+            CmdSetMrHide(1);
+            mrHide = 1;
+        }
+        else
+        {
+            CmdChangeSpeed(10.0f);
+            CmdSetMrHide(0);
+            mrHide = 0;
+        }
+    }
+
+    public void SendMessage(int value)
+    {
+        IntegerMessage myMessage = new IntegerMessage();
+        //getting the value of the input
+        myMessage.value = value;
+
+        //sending to server
+        NetworkManager.singleton.client.Send(chatMessage, myMessage);
+    }
+
+    private void ServerReceiveMessage(NetworkMessage message)
+    {
+        IntegerMessage myMessage = new IntegerMessage();
+        //we are using the connectionId as player name only to exemplify
+        myMessage.value = message.ReadMessage<IntegerMessage>().value;
+        //sending to all connected clients
+        NetworkServer.SendToAll(chatMessage, myMessage);
+    }
     // Use this for initialization
     protected void Start ()
     {
+        if (NetworkServer.active)
+        {
+            //registering the server handler
+            //NetworkServer.RegisterHandler(chatMessage, ServerReceiveMessage);
+        }
+        
+        NetworkManager.singleton.client.RegisterHandler(chatMessage, ReceiveMessage);
+        CmdInitScore();
         boxCollider = GetComponent<BoxCollider2D>();
         rb2D = GetComponent<Rigidbody2D>();
-        inverseMoveTime = 1f / moveTime;
+        inverseMoveTime = 10.0f;
         initSprite();
         initPosition();
-        timer = Random.Range(200, 300);
-        
+        timer = Random.Range(300, 700);
+        CmdSetCurrentId((int)netId.Value);
+        currentId = (int)netId.Value;
+
     }
 
-    void OnCollisionEnter()
+    void OnCollisionEnter(Collision col)
     {
         Destroy(gameObject);
     }
 
     private void initSprite()
     {
-        
-        sm = GetComponent<SpriteManager>();
         playerSpriter = Resources.LoadAll<Sprite>("Players");
+        enemySpriter = Resources.LoadAll<Sprite>("Enemy");
         int t = Random.Range(0, playerSpriter.Length);
-        sm.setSpriteNumber(t);
+        CmdChangeSprite(t);
         GetComponent<SpriteRenderer>().sprite = playerSpriter[t];
     }
 
-    private void initPosition()
+    public void initPosition()
     {
         int zone, q;
         Vector3 randomPosition = new Vector3(0, 0, 0);
@@ -101,27 +191,125 @@ public class Player : NetworkBehaviour
     {
         Vector2 start = transform.position;
         Vector2 end = start + new Vector2(xDir, yDir);
+        Player p;
 
         boxCollider.enabled = false;
         hit = Physics2D.Linecast(start, end, blockingLayer);
         boxCollider.enabled = true;
-
+        
         if (hit.transform == null)
         {
             moving = true;
             StartCoroutine(SmoothMovement(end));
             return true;
         }
-        else
+        else if (hit.collider.gameObject.GetComponent<Player>() != null)
+        {
+            p = hit.collider.gameObject.GetComponent<Player>();
+             if (mrHide == 1)
+             {
+                moving = true;
+                StartCoroutine(SmoothMovement(end));
+                CmdIncScore();
+                return true;
+            } else
+            {
+                return false;
+            }
+        } else
         {
             return false;
         }
+    }
+    void OnGUI()
+    {
+        Texture2D texture = new Texture2D(1, 1);
+        texture.SetPixel(0, 0, new Color(0,0,0));
+        texture.Apply();
+        GUI.skin.box.normal.background = texture;
+        GUI.Box(new Rect(50, 120, 100, 30), GUIContent.none);
+        GUI.Label(new Rect(50, 120, 100, 30), "Score: " + (score/2).ToString());
+    }
+    [Command]
+    public void CmdChangeSprite(int number)
+    {
+        playerLocalSprite = number;
+    }
+
+    [Command]
+    public void CmdSetMrHide(int number)
+    {
+        mrHide = number;
+    }
+
+    [Command]
+    public void CmdDecrementTime()
+    {
+        timer--;
+    }
+
+    [Command]
+    public void CmdChangeSpeed(float speed)
+    {
+        inverseMoveTime = speed;
     }
 
     // Update is called once per frame
     void Update () {
 
-        if(!isLocalPlayer)
+        if (mrHide == 0)
+        {
+            GetComponent<SpriteRenderer>().sprite = playerSpriter[playerLocalSprite];
+        } else
+        {
+            GetComponent<SpriteRenderer>().sprite = enemySpriter[0];
+        }
+
+        if (isServer && isLocalPlayer)
+        {
+            if (totalIds != NetworkManager.singleton.numPlayers)   // If a player connected
+            {
+                CmdSetTotalId(NetworkServer.connections.Count);
+            }
+        }
+            
+        if (isServer && isLocalPlayer)
+        {
+            CmdDecrementTime();
+            if (timer <= 0)
+            {
+                firstMessage = Random.Range(currentId, currentId + totalIds);
+
+                if (firstMessage == currentId)
+                {
+                    CmdChangeSpeed(20.0f);
+                    CmdSetMrHide(1);
+                    GetComponent<SpriteRenderer>().sprite = enemySpriter[0];
+                } else
+                {
+                    CmdChangeSpeed(10.0f);
+                    CmdSetMrHide(0);
+                    GetComponent<SpriteRenderer>().sprite = playerSpriter[playerLocalSprite];
+                }
+                // SendMessage(firstMessage);
+               
+                IntegerMessage myMessage = new IntegerMessage();
+                //we are using the connectionId as player name only to exemplify
+                myMessage.value = firstMessage;
+                //sending to all connected clients
+               // NetworkServer.SendToAll(chatMessage, myMessage);
+               // NetworkServer.SendToClient(firstMessage, chatMessage, myMessage);
+                foreach (NetworkConnection element in NetworkServer.connections)
+                {
+                    NetworkServer.SendToClient(element.connectionId, chatMessage, myMessage);
+                }
+
+                timer = Random.Range(300, 700);
+            }
+        }
+        
+
+        if (!isLocalPlayer)
         {
             return;
         }
